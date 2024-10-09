338bba715ddb34d5de4a688da1ca9b75965b6c0f
Kavit Trivedi <kavittrivedi@hotmail.com>
2024-10-09 17:05:47 +0530

    Initial created

---
+++ MVVMDemo/MVVMDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MVVMDemo.ViewModels;
+
+Console.WriteLine("Hello, World!");
+PersonViewModel viewModel = new PersonViewModel();
+
+Console.WriteLine("Initial Person Details:");
+Console.WriteLine($"Name: {viewModel.Name}, Age: {viewModel.Age}");
+
+// Update name and age
+Console.WriteLine("Enter new name:");
+viewModel.Name = Console.ReadLine();
+
+Console.WriteLine("Enter new age:");
+if (int.TryParse(Console.ReadLine(), out int newAge))
+{
+    viewModel.Age = newAge;
+}
+
+Console.WriteLine("Updated Person Details:");
+Console.WriteLine($"Name: {viewModel.Name}, Age: {viewModel.Age}");
---
+++ MVVMDemo/MVVMDemo/ViewModels/PersonViewModel.cs
@@ -0,0 +1,53 @@
+﻿using MVVMDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMDemo.ViewModels
+{
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        private Person _person;
+
+        public PersonViewModel()
+        {
+            _person = new Person { Name = "John Doe", Age = 30 };
+        }
+
+        public string Name
+        {
+            get => _person.Name;
+            set
+            {
+                if (_person.Name != value)
+                {
+                    _person.Name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        public int Age
+        {
+            get => _person.Age;
+            set
+            {
+                if (_person.Age != value)
+                {
+                    _person.Age = value;
+                    OnPropertyChanged(nameof(Age));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
